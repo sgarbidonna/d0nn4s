e63b68c7101f3b33d2381284589f80f174ee747a
D0NN4 <sgarbidonna@gmail.com>
2026-03-09 21:33:34 -0300

    init II

--- assets/js/05-remove-img-attrs.js
+++
@@ -1,8 +0,0 @@
-/* Elimina atributos title y alt de imágenes */
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelectorAll('img').forEach(img => {
-    img.removeAttribute('title');
-    img.removeAttribute('alt');
-  });
-});
